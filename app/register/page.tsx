0179e2a2a269e6bfff1f9a7a880c06e3d95e4cbb
alswns0340 <alswns5870@naver.com>
2025-02-21 14:20:54 +0900

    ✨ feat: 회원가입 80%구현완료 , 🐛 bug: login fetch 수정

---
+++ app/register/page.tsx
@@ -0,0 +1,21 @@
+import Register from "../component/register";
+
+
+export default function registerPage() {
+
+  return (
+    <div className="min-h-screen flex flex-col bg-nihonred">
+      {/* 상단 배경 */}
+      <div className="w-full flex items-center justify-left ml-[5%] text-white text-5xl font-bold text-left p-6 whitespace-nowrap">
+        <div>
+          SignUp
+        </div>
+      </div>
+
+      {/* 회원가입입 폼 */}
+      <div className="flex-[3] bg-gray-100 flex items-center p-4 px-10 rounded-tr-[200px]">
+        <Register/>
+      </div>
+    </div>
+  );
+}
